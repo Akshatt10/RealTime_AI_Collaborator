fbd921217cdc51581bc5d0202f7e38ed25de0ae4
Akshatt10 <akshattyagi1010@gmail.com>
2025-02-20 00:30:54 +0530

    Initialized the project

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import morgan from 'morgan';
+import express from 'express';
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default app;
