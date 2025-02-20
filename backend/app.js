075731661efa3e0f092912053ea0170aa8996569
Akshatt10 <akshattyagi1010@gmail.com>
2025-02-20 23:51:18 +0530

    Database connection

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 import morgan from 'morgan';
 import express from 'express';
+import connect from './db/db.js';
 
+
+connect();
 
 const app = express();
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
