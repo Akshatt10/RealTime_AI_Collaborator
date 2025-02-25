557cbae331e194f86b816c3daa1ad244411a176a
Akshatt10 <akshattyagi1010@gmail.com>
2025-02-25 18:06:31 +0530

    Frontend routing

---
+++ frontend/src/screens/Register.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+const Register = () => {
+
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    function submitHandler(e) {
+        e.preventDefault()
+    }
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold text-white mb-6 text-center">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-400 mb-2" htmlFor="email">Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              className="w-full p-3 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-400 mb-2" htmlFor="password">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              className="w-full p-3 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600 transition duration-200"
+          >
+            Login
+          </button>
+        </form>
+        <p className="text-gray-400 mt-6 text-center">
+         Already have an account? <Link to="/login" className="text-blue-500 hover:underline">Login</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Register
