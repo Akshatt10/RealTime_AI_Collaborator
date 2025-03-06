6d012f1f1eded3d7f807a2625fdb27ba1826cfd2
Akshatt10 <akshattyagi1010@gmail.com>
2025-03-06 23:01:59 +0530

    Context hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoute'
+import { UserProvider } from './context/user.context'
 
 const App = () => {
   return (
-   <AppRoutes/>
+    <UserProvider>
+      <AppRoutes/>
+    </UserProvider>
   )
 }
 
